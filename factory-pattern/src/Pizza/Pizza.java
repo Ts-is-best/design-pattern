700b86deadce5f55a6ea72cdfaaa59a2a4535444
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-09-26 18:58:11 +0900

    Add fields and detailed preparation steps to Pizza class
    Introduce fields for name, dough, sauce, and toppings. Implement detailed output in the prepare, bake, cut, and box methods to enhance traceability of the pizza-making process.

--- factory-pattern/src/Pizza/Pizza.java
+++ factory-pattern/src/Pizza/Pizza.java
@@ -1,16 +1,38 @@
 package Pizza;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pizza {
+    protected String name;
+    protected String dough;
+    protected String sauce;
+    protected List<String> toppings = new ArrayList<String>();
+
     public void prepare() {
+        System.out.println("Preparing " + name);
+        System.out.println("Tossing dough... " + dough);
+        System.out.println("Adding sauce... " + sauce);
+        System.out.println("Adding toppings: ");
+        for (String topping : toppings) {
+            System.out.println("  " + topping);
+        }
     }
 
     public void bake() {
+        System.out.println("Bake for 25 minutes at 350");
     }
 
     public void cut() {
+        System.out.println("Cutting the pizza into diagonal slices");
     }
 
     public void box() {
+        System.out.println("Place pizza in official PizzaStore box");
+    }
+
+    public String getName() {
+        return name;
     }
 }
 
