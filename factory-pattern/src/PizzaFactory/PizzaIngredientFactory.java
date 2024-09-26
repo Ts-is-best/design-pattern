584cc04c3c5139069eca44289fdcb10a9dce95ae
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-09-26 19:17:12 +0900

    Add PizzaIngredientFactory interface
    Introduce the PizzaIngredientFactory interface to define methods for creating various pizza ingredients. This facilitates consistent ingredient creation and enhances modularity in the pizza-making process.

---
+++ factory-pattern/src/PizzaFactory/PizzaIngredientFactory.java
@@ -0,0 +1,13 @@
+package PizzaFactory;
+
+import Ingredient.*;
+
+public interface PizzaIngredientFactory {
+    public Dough createDough();
+    public Sauce createSauce();
+    public Cheese createCheese();
+    public Veggies[] createVeggies();
+    public Pepperoni createPepperoni();
+    public Clams createClam();
+}
+
