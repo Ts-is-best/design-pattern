b8c1770dc54a147dfb9933fd743880e951bde9a9
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-10-03 18:51:53 +0900

    ✨(feat) Add undo functionality to RemoteControl
    Introduce an undoCommand to store the last executed command. Implement the undoButtonWasPushed method to revert the last executed command, enhancing the remote control's functionality.

--- command-pattern/src/RemoteControl.java
+++ command-pattern/src/RemoteControl.java
@@ -1,6 +1,7 @@
 public class RemoteControl {
     Command[] onCommands;
     Command[] offCommands;
+    Command undoCommand;
 
     public RemoteControl() {
         onCommands = new Command[7];
@@ -11,6 +12,7 @@ public class RemoteControl {
             onCommands[i] = noCommand;
             offCommands[i] = noCommand;
         }
+        undoCommand = noCommand;
     }
 
     public void setCommand(int slot, Command onCommand, Command offCommand) {
@@ -20,10 +22,16 @@ public class RemoteControl {
 
     public void onButtonWasPushed(int slot) {
         onCommands[slot].execute();
+        undoCommand = onCommands[slot];
     }
 
     public void offButtonWasPushed(int slot) {
         offCommands[slot].execute();
+        undoCommand = offCommands[slot];
+    }
+
+    public void undoButtonWasPushed() {
+        undoCommand.undo();
     }
 
     public String toString() {
@@ -39,6 +47,7 @@ public class RemoteControl {
                     .append(offCommands[i].getClass().getName())
                     .append("\n");
         }
+        stringBuilder.append("[undo] ").append(undoCommand.getClass().getName());
         return stringBuilder.toString();
     }
 }
