2f98ae3cad5f10599d35fd46a5bc7da28055cf6d
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-10-03 18:15:11 +0900

    ✨(feat) Add RemoteLoader with basic light commands.
    Introduce the RemoteLoader class which configures and tests basic light commands for living room and kitchen. This includes setting up on and off commands for each light and testing them through the RemoteControl interface.

---
+++ command-pattern/src/RemoteLoader.java
@@ -0,0 +1,23 @@
+public class RemoteLoader {
+    public static void main(String[] args) {
+        RemoteControl remoteControl = new RemoteControl();
+
+        Light livingRoomLight = new Light("거실");
+        Light kitchenLight = new Light("주방");
+
+        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+        LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
+        LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
+
+        remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+        remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
+
+        System.out.println(remoteControl);
+
+        remoteControl.onButtonWasPushed(0);
+        remoteControl.offButtonWasPushed(0);
+        remoteControl.onButtonWasPushed(1);
+        remoteControl.offButtonWasPushed(1);
+    }
+}
