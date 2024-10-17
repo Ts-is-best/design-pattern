6f8cdcbfb19891a13a0302eef7f3ca9463173e85
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-10-17 20:15:18 +0900

    📝 (test) Add LegacyGumballMachineTestDrive class
    Introduce `LegacyGumballMachineTestDrive` to test various operations on `LegacyGumballMachine` such as inserting quarters and turning the crank. This will help in verifying the machine's behavior in conjunction with the user interactions.

---
+++ state-pattern/src/LegacyGumballMachineTestDrive.java
@@ -0,0 +1,36 @@
+public class LegacyGumballMachineTestDrive {
+    public static void main(String[] args) {
+        LegacyGumballMachine gumballMachine = new LegacyGumballMachine(5);
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.ejectQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.ejectQuarter();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+    }
+}
